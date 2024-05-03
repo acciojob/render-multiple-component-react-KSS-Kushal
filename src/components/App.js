0e4a735d17e02ba18c43e73f8336df83a458911c
Kushal Ghosh <kushalghosh075@gmail.com>
2024-05-03 22:24:55 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Card from "./Card";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Card name={"Abc"} description={"xyz"} />
+      <Card name={"rdf"} description={"lhkjhk"} />
+    </div>
   )
 }
 
